0c2106d71a5f048fa76250554f0fd937547e46bb
suzukihiroto <suzukihiroto.tabletennis@gmail.com>
2019-02-11 02:16:49 +0900

    Add Bootstrap4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "PAMER ~学生限定国際交流マッチングアプリ~"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
