1329e295a1f1966d0a2c096bbea7257bc5b50ef7
Yuya Ishii <yypswm61@gmail.com>
2019-02-11 10:22:29 +0000

    migrate reset

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20190211090933) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.boolean "guide"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
