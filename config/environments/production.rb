c8196ca6e381ddc6af2b705b0fb130764fe0fdc4
suzukihiroto <suzukihiroto.tabletennis@gmail.com>
2019-03-03 15:53:39 +0900

    heroku preparation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = ENV['HOST_HEROKU']
-  config.action_mailer.default_url_options = { :host => ENV['HOST_HEROKU'] }
+  host = 'pamer.herokuapp.com'
+  # Rails.application.routes.default_url_options[:host] = host
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
